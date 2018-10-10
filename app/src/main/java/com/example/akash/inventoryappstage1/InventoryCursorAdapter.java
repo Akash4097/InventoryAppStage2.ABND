6b0028ade08a026aed7f86f6c57b1ed3ac92b152
Akash Singh <goandsatish@gmail.com>
2018-10-10 10:30:48 +0530

    before implementing loaders

--- app/src/main/java/com/example/akash/inventoryappstage1/InventoryCursorAdapter.java
+++ app/src/main/java/com/example/akash/inventoryappstage1/InventoryCursorAdapter.java
@@ -6,19 +6,22 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 
 import com.example.akash.inventoryappstage1.data.InventoryContract;
 
-public class InventoryCursorAdapter extends RecyclerView.Adapter<InventoryCursorAdapter.ViewHolder> {
+public class InventoryCursorAdapter extends RecyclerView.Adapter<InventoryCursorAdapter.ViewHolder>{
 
     CursorAdapter cursorAdapter;
     Context context;
 
     public InventoryCursorAdapter(Context context, Cursor cursor){
         this.context = context;
-        cursorAdapter = new CursorAdapter(context,cursor,0) {
+        cursorAdapter = new CursorAdapter(context,cursor){
+
             @Override
             public View newView(Context context, Cursor cursor, ViewGroup parent) {
                 return LayoutInflater.from(context).inflate(R.layout.list_item,parent,false);
@@ -29,21 +32,23 @@ public class InventoryCursorAdapter extends RecyclerView.Adapter<InventoryCursor
 
                 TextView name = view.findViewById(R.id.productNameTextView);
                 TextView price = view.findViewById(R.id.productPriceTextView);
+                TextView quantity = view.findViewById(R.id.productQuantityTextView);
 
                 String productName = cursor.getString(cursor.getColumnIndexOrThrow(InventoryContract.ProductEntry.COLUMN_PRODUCT_NAME));
                 int productPrice = cursor.getInt(cursor.getColumnIndexOrThrow(InventoryContract.ProductEntry.COLUMN_PRICE));
+                int productQuantity = cursor.getInt(cursor.getColumnIndexOrThrow(InventoryContract.ProductEntry.COLUMN_QUANTITY));
+                int id = cursor.getInt(cursor.getColumnIndexOrThrow(InventoryContract.ProductEntry._ID));
 
-                name.setText(productName);
+                quantity.setText(String.valueOf(productQuantity));
+                name.setText( String.valueOf(id)+ ":" + productName);
                 price.setText(String.valueOf(productPrice));
-
             }
         };
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
-        public ViewHolder(View itemView){
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
             super(itemView);
-
         }
     }
 
--- app/src/main/java/com/example/akash/inventoryappstage1/MainActivity.java
+++ app/src/main/java/com/example/akash/inventoryappstage1/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.empty_view)
     RelativeLayout emptyView;
 
+    private static final int URL_LOADER = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
